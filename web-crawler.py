66b28bd45c78d85e0e28a9187182be3f27c1bbc0
mvillard84 <74992391+mvillard84@users.noreply.github.com>
2024-03-09 17:38:38 -0300

    Update web-crawler.py

--- web-crawler.py
+++ web-crawler.py
@@ -3,24 +3,24 @@ from bs4 import BeautifulSoup
 import csv
 
 
-def simple_crawler(url, output_file="links.csv"):
+def web_crawler(url, output_file="links.csv"):
     try:
         response = requests.get(url)
         response.raise_for_status()  # Verificar si hay errores en la solicitud
         html_content = response.text
 
-        # Analizar el contenido HTML con BeautifulSoup
+        # Analizar el contenido
         soup = BeautifulSoup(html_content, 'html.parser')
 
-        # Guardar los enlaces en un archivo CSV
+        # Guardar los links en un archivo CSV
         with open(output_file, 'a', newline='', encoding='utf-8') as csvfile:
             fieldnames = ['Title', 'URL']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-            if csvfile.tell() == 0:  # Si el archivo está vacío, escribir encabezados
+            if csvfile.tell() == 0:  # Si el archivo está vacío, escribe la cabecera
                 writer.writeheader()
 
-            # Obtener títulos (h1 a h6) y enlaces
+            # Obtiene los headers (h1 a h6) y sus links
             for i in range(1, 7):
                 titles = soup.find_all(f'h{i}')
                 for title in titles:
@@ -28,23 +28,25 @@ def simple_crawler(url, output_file="links.csv"):
                     link = title.find('a')
                     link_url = link.get('href') if link else "No link available"
 
-                    # Imprimir y guardar en el archivo CSV
+                    # Imprimir en pantalla y guarda el resultado en un archivo CSV
                     print(f"Title: {title_text}")
                     print(f"URL: {link_url}")
                     writer.writerow({'Title': title_text, 'URL': link_url})
 
-            # Obtener enlaces y rastrear recursivamente
+            # Obtiene los links del sitio y los rastrea en forma recursiva
             links = soup.find_all('a')
             for link in links:
                 new_url = link.get('href')
                 if new_url and new_url.startswith('http'):
-                    simple_crawler(new_url, output_file)
+                    web_crawler(new_url, output_file)
 
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
 
 
-start_url = "[SITE-URL]"
+# Parámetros
+target_url = "[SITE-URL]"
 output_file = "links.csv"
 
-simple_crawler(start_url, output_file)
+# Ejecución del web crawler
+web_crawler(target_url, output_file)
